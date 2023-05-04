04105ae0c8849bd1ed47e731b408baadefdbbd38
Igor Anufriev <dev@sacmi5.ru>
2023-05-05 02:22:40 +0700

    m3l1: добавил скриншоты

--- module_3/lesson_1/syscalls.c
+++ module_3/lesson_1/syscalls.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
 
-void main() {
-    printf("%d\n", getpid());
-    printf("%d\n", getppid());
-    printf("%d\n", getuid());
-    printf("%d\n", geteuid());
-    printf("%d\n", getgid());
+int main() {
+  printf("%d\n", getpid());
+  printf("%d\n", getppid());
+  printf("%d\n", getuid());
+  printf("%d\n", geteuid());
+  printf("%d\n", getgid());
 
+  return 0;
 }
