8b0856e363d0cb6316eb6ca91d09cf6131b12aac
Igor Anufriev <dev@sacmi5.ru>
2023-05-05 03:37:34 +0700

    m3l2: скриншоты, описание
    также поменял местами приоритеты в `2.10`

--- module_3/lesson_2/p_reader.c
+++ module_3/lesson_2/p_reader.c
@@ -1,49 +1,49 @@
 #include <mqueue.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define QUEUE "/brand_new_queue"
 #define MSG_SIZE 255
 
 int main(void) {
-    mqd_t ds;
-    struct mq_attr attr;
+  mqd_t ds;
+  struct mq_attr attr;
 
-    if ((ds = mq_open(QUEUE, O_CREAT | O_RDONLY, 0600, NULL)) == -1) {
-        perror("mq_open");
-        exit(1);
+  if ((ds = mq_open(QUEUE, O_CREAT | O_RDONLY, 0600, NULL)) == -1) {
+    perror("mq_open");
+    exit(1);
+  }
+
+  if (mq_getattr(ds, &attr) == -1) {
+    perror("mq_getattr");
+    exit(1);
+  }
+
+  char *message = calloc(attr.mq_msgsize, sizeof(char));
+  while (1) {
+    unsigned int prio;
+    if (mq_receive(ds, message, attr.mq_msgsize, &prio) == -1) {
+      perror("mq_receive");
+      exit(1);
     }
 
-    if (mq_getattr(ds, &attr) == -1) {
-        perror("mq_getattr");
-        exit(1);
+    if (prio == 1) {
+      printf("Получен сигнал выхода\n");
+      break;
     }
 
-    char *message = calloc(attr.mq_msgsize, sizeof(char));
-    while (1) {
-        unsigned int prio;
-        if (mq_receive(ds, message, attr.mq_msgsize, &prio) == -1) {
-            perror("mq_receive");
-            exit(1);
-        }
+    printf("%d - %s\n", prio, message);
+  }
 
-        if (prio == 255) {
-            printf("Получен сигнал выхода\n");
-            break;
-        }
+  if (mq_close(ds) == -1) {
+    perror("mq_close");
+    exit(1);
+  }
 
-        printf("%d - %s\n", prio, message);
-    }
+  if (mq_unlink(QUEUE) == -1) {
+    perror("mq_unlink");
+    exit(1);
+  }
 
-    if (mq_close(ds) == -1) {
-        perror("mq_close");
-        exit(1);
-    }
-
-    if (mq_unlink(QUEUE) == -1) {
-        perror("mq_unlink");
-        exit(1);
-    }
-
-    return 0;
+  return 0;
 }
--- module_3/lesson_2/p_writer.c
+++ module_3/lesson_2/p_writer.c
@@ -1,52 +1,53 @@
 #include <mqueue.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #define QUEUE "/brand_new_queue"
 #define MSG_SIZE 256
-#define MSG_PRIORITY 1
-#define END_PRIORITY 255
+#define MSG_PRIORITY 255
+#define END_PRIORITY 1
 
 int main(void) {
-    mqd_t ds;
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 6;
-    attr.mq_msgsize = MSG_SIZE;
-    attr.mq_curmsgs = 0;
+  mqd_t ds;
+  struct mq_attr attr;
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = 6;
+  attr.mq_msgsize = MSG_SIZE;
+  attr.mq_curmsgs = 0;
 
-    if ((ds = mq_open(QUEUE, O_CREAT | O_WRONLY | O_NONBLOCK, 0666, &attr)) == -1) {
-        perror("mq_open");
-        exit(1);
+  if ((ds = mq_open(QUEUE, O_CREAT | O_WRONLY | O_NONBLOCK, 0666, &attr)) ==
+      -1) {
+    perror("mq_open");
+    exit(1);
+  }
+
+  char message[MSG_SIZE];
+  srand(time(NULL));
+
+  for (int i = 0; i < 5; ++i) {
+    int random = rand();
+    snprintf(message, MSG_SIZE, "%d", random);
+
+    if (mq_send(ds, message, strlen(message), MSG_PRIORITY) == -1) {
+      perror("mq_send");
+      exit(1);
     }
+  }
 
-    char message[MSG_SIZE];
-    srand(time(NULL));
+  sleep(1);
 
-    for (int i = 0; i < 5; ++i) {
-        int random = rand();
-        snprintf(message, MSG_SIZE, "%d", random);
+  if (mq_send(ds, "", 0, END_PRIORITY) == -1) {
+    perror("mq_send");
+    exit(1);
+  }
 
-        if (mq_send(ds, message, strlen(message), MSG_PRIORITY) == -1) {
-            perror("mq_send");
-            exit(1);
-        }
-    }
+  if (mq_close(ds) == -1) {
+    perror("mq_close");
+    exit(1);
+  }
 
-    sleep(1);
-
-    if (mq_send(ds, "", 0, END_PRIORITY) == -1) {
-        perror("mq_send");
-        exit(1);
-    }
-
-    if (mq_close(ds) == -1) {
-        perror("mq_close");
-        exit(1);
-    }
-
-    return 0;
+  return 0;
 }
