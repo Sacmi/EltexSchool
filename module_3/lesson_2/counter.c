dee480d7ef8951cbe43d7ee6f0e35880f032852f
Igor Anufriev <dev@sacmi5.ru>
2023-05-05 02:54:20 +0700

    m3l2: добавил описание

--- module_3/lesson_2/counter.c
+++ module_3/lesson_2/counter.c
@@ -4,6 +4,17 @@
 
 #define FILE_PATH "/tmp/counter"
 
+/* 
+	При SIGINT процесс завершается и выдает Interrupt
+	При SIGQUIT процесс завершается и выдает Quit
+	При SIGABRT процесс завершается и выдает Aborted
+	При SIGKILL процесс завершается и выдает Killed
+	При SIGTERM процесс завершается и выдает Terminated
+	При SIGTSTP процесс останавливается и выдает Stopped 
+	При SIGSTOP процесс останавливается и выдает Stoppped
+	При SIGCONT процесс восстанавливается
+*/ 
+
 int main() {
     size_t counter = 0;
     FILE *file = fopen(FILE_PATH, "w");
