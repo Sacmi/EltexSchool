7796c3521f182672c135ef7160c06891f275478e
Igor Anufriev <dev@sacmi5.ru>
2023-05-11 07:27:38 +0000

    m5l2: задание 1 - рефакторинг кода

--- module_5/lesson_2/task_1/proc_module.c
+++ module_5/lesson_2/task_1/proc_module.c
@@ -5,39 +5,43 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
-static int len = 0, maxLen = 64, unreaded = 0;
+#define SIZE 64
 
-static char *msg;
+static int length = 0, maximum_length = 64, unreaded_length = 0;
+
+static char *message;
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Igor Anufriev");
 MODULE_DESCRIPTION("A module that works with `/proc/` directory");
 
 ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp) {
-  int notCopied;
-  if (unreaded == 0) {
-    unreaded = len;
+  if (unreaded_length == 0) {
+    unreaded_length = length;
     return 0;
   }
-  if (count > unreaded) {
-    count = unreaded;
+  
+  if (count > unreaded_length) {
+    count = unreaded_length;
   }
-  notCopied = (int)copy_to_user(buf, msg, count);
-  unreaded -= count - notCopied;
 
-  return count - notCopied;
+  int not_copied = copy_to_user(buf, message, count);
+  unreaded_length -= count - not_copied;
+
+  return count - not_copied;
 }
 
 ssize_t write_proc(struct file *filp, const char *buf, size_t count,
                    loff_t *offp) {
-  int notCopied;
-  if (count > maxLen) {
-    count = maxLen;
+  if (count > maximum_length) {
+    count = maximum_length;
   }
-  notCopied = (int)copy_from_user(msg, buf, count);
-  len = count - notCopied;
-  unreaded = len;
-  return len;
+
+  int not_copied = (int)copy_from_user(message, buf, count);
+  length = count - not_copied;
+  unreaded_length = length;
+  
+  return length;
 }
 
 const struct proc_ops proc_fops = {.proc_read = read_proc,
@@ -45,13 +49,13 @@ const struct proc_ops proc_fops = {.proc_read = read_proc,
 
 int __init proc_init(void) {
   proc_create("hello", 0666, NULL, &proc_fops);
-  msg = kmalloc(64 * sizeof(char), GFP_KERNEL);
+  message = kmalloc(SIZE * sizeof(char), GFP_KERNEL);
   return 0;
 }
 
 void __exit proc_cleanup(void) {
   remove_proc_entry("hello", NULL);
-  kfree(msg);
+  kfree(message);
 }
 
 module_init(proc_init);
